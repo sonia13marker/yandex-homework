2e8758b4699cc6a68d34ea8da5b929ff4e3f0771
sonia13marker <soniamarker13@gmail.com>
2022-11-11 15:05:34 +0300

    update button cart

--- 3-4/src/components/Layout/Layout.jsx
+++ 3-4/src/components/Layout/Layout.jsx
@@ -5,7 +5,7 @@ import styles from './Layout.css'
 export  const Layout = (props) => { 
 
 	return <div>
-		<header className="header"><h1 className="header__title">Магазин</h1></header>
+		<header className="header"><h1 className="header__title">Магазин</h1><button className="button__cart"><img src={require('./shopping-cart.png')}/></button></header>
 		<div>
 			{ props.children }
 		</div>
