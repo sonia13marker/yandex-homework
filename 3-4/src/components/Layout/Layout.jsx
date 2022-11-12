76b3b685b9e616d9ec9a48d434e5ce2beb1e88e0
sonia13marker <soniamarker13@gmail.com>
2022-11-12 12:52:15 +0300

    обнова

--- 3-4/src/components/Layout/Layout.jsx
+++ 3-4/src/components/Layout/Layout.jsx
@@ -2,14 +2,21 @@
 
 import styles from './Layout.css';
 import { Link } from 'react-router-dom';
+import {BookshelfPage} from '../../pages/BookshelfPage/BookshelfPage.jsx';
 
 export  const Layout = (props) => { 
 
 	return <div>
 		<header className="header">
 			<Link to="/"><h1 className="header__title">Магазин</h1></Link>
-			<Link to="bookshelfs">Bookshelfs</Link>
+			<Link to="/bookshelf">
+				{
+					<BookshelfPage/>
+				}
+			</Link>
+			<Link to="/cart">
 			<button className="button__cart"><img src={require('./shopping-cart.png')}/></button>
+			</Link>
 		</header>
 		<div>
 			{ props.children }
