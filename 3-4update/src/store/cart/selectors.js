aeda2cfdfb4311049d9a5af4f79b796a81e3fee4
sonia13marker <111881249+sonia13marker@users.noreply.github.com>
2022-11-15 13:24:23 +0300

    Delete 3-4update directory

--- 3-4update/src/store/cart/selectors.js
+++
@@ -1,3 +0,0 @@
-export const selectCartModule = (state) => state.cart;
-
-export const selectBookCount = (state, bookId) => selectCartModule(state)[bookId];
--- 3-4update/src/store/utils.js
+++
@@ -1,9 +0,0 @@
-export const prepareData = (items) => {
-	// у него acc[cinema.id]
-	return {
-		entities: items.reduce((acc, books) => {
-			acc[books.id] = books;
-		}, {}),
-		ids: items.map(({id}) => id),
-	}
-} 
